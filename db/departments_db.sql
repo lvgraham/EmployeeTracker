94a24588665fa5bf5bbc1e21105584b928ae91b4
lvgraham <liavibanez@gmail.com>
2020-07-12 19:27:05 -0500

    adding employee db

--- db/departments_db.sql
+++ db/departments_db.sql
@@ -5,16 +5,15 @@ CREATE DATABASE departments_DB;
 USE departments_DB;
 
 CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
 );
 
-INSERT INTO  departments ('name')
+INSERT INTO  departments (name)
 VALUES ();
 
-INSERT INTO  departments ('name')
+INSERT INTO  departments (name)
 VALUES ();
 
-INSERT INTO  departments ('name')
+INSERT INTO  departments (name)
 VALUES ();
